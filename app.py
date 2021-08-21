deba12a102aeae6c0fbb49b9bb4058dbdd75defc
Albérico de Castro <alberico.castro@gethelix.org>
2021-08-21 12:37:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Bem vindo a Gulliver Traveller!'
+    return 'Let\'s Rock the Future!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
